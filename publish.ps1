3578fe694bb497e2aa5db3c7ec48638c5cd214e2
Don Kackman <dkackman@gmail.com>
2023-11-22 11:56:01 -0600

    StripSymbols

--- publish.ps1
+++ publish.ps1
@@ -13,11 +13,11 @@ function Publish-Project {
     )
 
     # single standalone file
-    dotnet publish ./$src/$name.csproj -c Release -r $runtime --framework $framework --self-contained true /p:Version=$version /p:PublishReadyToRun=true /p:PublishSingleFile=True /p:PublishTrimmed=false /p:IncludeNativeLibrariesForSelfExtract=True /p:PublishDir="bin\Release\$framework\$runtime\" --output $outputRoot/standalone/$runtime
+    dotnet publish ./$src/$name.csproj -c Release -r $runtime --framework $framework --self-contained true /p:Version=$version /p:PublishReadyToRun=true /p:PublishSingleFile=True /p:PublishTrimmed=false /p:IncludeNativeLibrariesForSelfExtract=True /p:StripSymbols=true /p:PublishDir="bin\Release\$framework\$runtime\" --output $outputRoot/standalone/$runtime
     Compress-Archive -CompressionLevel Optimal -Path $outputRoot/standalone/$runtime/* -DestinationPath $outputRoot/$name-$version-standalone-$runtime.zip
 
     # single file that needs dotnet installed
-    dotnet publish ./$src/$name.csproj -c Release -r $runtime --framework $framework --self-contained false /p:Version=$version /p:PublishReadyToRun=false /p:PublishSingleFile=True /p:PublishTrimmed=false /p:IncludeNativeLibrariesForSelfExtract=True /p:PublishDir="bin\Release\$framework\$runtime\" --output $outputRoot/singlefile/$runtime
+    dotnet publish ./$src/$name.csproj -c Release -r $runtime --framework $framework --self-contained false /p:Version=$version /p:PublishReadyToRun=false /p:PublishSingleFile=True /p:PublishTrimmed=false /p:IncludeNativeLibrariesForSelfExtract=True /p:StripSymbols=true /p:PublishDir="bin\Release\$framework\$runtime\" --output $outputRoot/singlefile/$runtime
     Compress-Archive -CompressionLevel Optimal -Path $outputRoot/singlefile/$runtime/* -DestinationPath $outputRoot/$name-$version-singlefile-$runtime.zip
 }
 
@@ -26,7 +26,7 @@ Publish-Project("linux-x64")
 Publish-Project("osx-x64")
 
 if ([System.Environment]::OSVersion.Platform -eq "Win32NT") {
-    # build the msi - win-x64 only for now
+    # build the msi - win-x64 only
     dotnet build ./MsiInstaller/MsiInstaller.wixproj -c Release -r win-x64 --output $outputRoot
     Move-Item -Path $outputRoot/en-us/*.msi -Destination $outputRoot/$name-$version-service-win-x64.msi
 }
